9a1894eec2b87d9ef1f380a93f8041253f0f97fa
Paul <paul-poco@hotmail.fr>
2025-08-07 09:22:44 -0400

    Blob trigger

--- azf-process-files/function_app.py
+++ azf-process-files/function_app.py
@@ -1,8 +1,10 @@
 import azure.functions as func
+from azure.storage.blob import BlobServiceClient
 import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
+@app.function_name(name="http_trigger")
 @app.route(route="http_trigger", methods=["GET", "POST"], auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -23,4 +25,12 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+    
+@app.function_name(name="get_file")
+@app.blob_trigger(arg_name="stinternalrag001", path="content", connection="AzureWebJobsStorage")
+def get_file(my_blob: func.InputStream) -> func.HttpResponse:
+    logging.info("Blob trigger function processed a blob.")
 
+    file_name = my_blob.name
+
+    return func.HttpResponse(f"Hello, {name}. This Blob triggered function executed successfully.")
